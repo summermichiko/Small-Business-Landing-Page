9ba5fd2121fe1e653672cb162683dff2da0f4d69
Summer <summermichiko@gmail.com>
2014-05-21 02:12:13 -0400

    updated site

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
 			$(".mainMenu").removeClass("fixed");
 		}
 	}; 
+
 	$(window).scroll(function() {
 		fixedMenu();
 	});
@@ -17,21 +18,7 @@ $(document).ready(function() {
 	$("#branchStudios").click(function() {
 		window.scrollTo(0,0);
 	});
-	$("#about").click(function() {
-		window.scrollTo(0,531);
-	});
-	$("#school").click(function() {
-		window.scrollTo(0, 1176);
-	});
-	$("#courses").click(function() {
-		window.scrollTo(0, 2161);
-	});
-	$("#events").click(function() {
-		window.scrollTo(0, 2806);
-	});
-	$("#contact").click(function() {
-		window.scrollTo(0, 4031);
-	});
+	
 }); 
 
 
